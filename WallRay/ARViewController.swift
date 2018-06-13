2e8ef629d1accb07bb924c4f37456b07aa178e7b
René Finzel <r.finzel@outlook.de>
2018-06-13 12:29:14 +0200

    Erweiterung der Vector-klasse (unnötige Methoden sollten noch entfernt werden, sowie Quellenangabe) Versuch Linien zu Zeichen aber werden funktioniert noch nicht ganz

--- WallRay/ARViewController.swift
+++ WallRay/ARViewController.swift
@@ -18,6 +18,245 @@ extension SCNGeometry {
         return SCNGeometry(sources: [source], elements: [element])
     }
 }
+extension SCNVector3
+{
+    /**
+     * Negates the vector described by SCNVector3 and returns
+     * the result as a new SCNVector3.
+     */
+    func negate() -> SCNVector3 {
+        return self * -1
+    }
+    
+    /**
+     * Negates the vector described by SCNVector3
+     */
+    mutating func negated() -> SCNVector3 {
+        self = negate()
+        return self
+    }
+    
+    /**
+     * Returns the length (magnitude) of the vector described by the SCNVector3
+     */
+    func length() -> Float {
+        return sqrtf(x*x + y*y + z*z)
+    }
+    
+    /**
+     * Normalizes the vector described by the SCNVector3 to length 1.0 and returns
+     * the result as a new SCNVector3.
+     */
+    func normalized() -> SCNVector3 {
+        return self / length()
+    }
+    
+    /**
+     * Normalizes the vector described by the SCNVector3 to length 1.0.
+     */
+    mutating func normalize() -> SCNVector3 {
+        self = normalized()
+        return self
+    }
+    
+    /**
+     * Calculates the distance between two SCNVector3. Pythagoras!
+     */
+    func distance(vector: SCNVector3) -> Float {
+        return (self - vector).length()
+    }
+    
+    /**
+     * Calculates the dot product between two SCNVector3.
+     */
+    func dot(vector: SCNVector3) -> Float {
+        return x * vector.x + y * vector.y + z * vector.z
+    }
+    
+    /**
+     * Calculates the cross product between two SCNVector3.
+     */
+    func cross(vector: SCNVector3) -> SCNVector3 {
+        return SCNVector3Make(y * vector.z - z * vector.y, z * vector.x - x * vector.z, x * vector.y - y * vector.x)
+    }
+}
+
+/**
+ * Adds two SCNVector3 vectors and returns the result as a new SCNVector3.
+ */
+func + (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
+
+/**
+ * Increments a SCNVector3 with the value of another.
+ */
+func += ( left: inout SCNVector3, right: SCNVector3) {
+    left = left + right
+}
+
+/**
+ * Subtracts two SCNVector3 vectors and returns the result as a new SCNVector3.
+ */
+func - (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x - right.x, left.y - right.y, left.z - right.z)
+}
+
+/**
+ * Decrements a SCNVector3 with the value of another.
+ */
+func -= ( left: inout SCNVector3, right: SCNVector3) {
+    left = left - right
+}
+
+/**
+ * Multiplies two SCNVector3 vectors and returns the result as a new SCNVector3.
+ */
+func * (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x * right.x, left.y * right.y, left.z * right.z)
+}
+
+/**
+ * Multiplies a SCNVector3 with another.
+ */
+func *= ( left: inout SCNVector3, right: SCNVector3) {
+    left = left * right
+}
+
+/**
+ * Multiplies the x, y and z fields of a SCNVector3 with the same scalar value and
+ * returns the result as a new SCNVector3.
+ */
+func * (vector: SCNVector3, scalar: Float) -> SCNVector3 {
+    return SCNVector3Make(vector.x * scalar, vector.y * scalar, vector.z * scalar)
+}
+
+/**
+ * Multiplies the x and y fields of a SCNVector3 with the same scalar value.
+ */
+func *= ( vector: inout SCNVector3, scalar: Float) {
+    vector = vector * scalar
+}
+
+/**
+ * Divides two SCNVector3 vectors abd returns the result as a new SCNVector3
+ */
+func / (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x / right.x, left.y / right.y, left.z / right.z)
+}
+
+/**
+ * Divides a SCNVector3 by another.
+ */
+func /= ( left: inout SCNVector3, right: SCNVector3) {
+    left = left / right
+}
+
+/**
+ * Divides the x, y and z fields of a SCNVector3 by the same scalar value and
+ * returns the result as a new SCNVector3.
+ */
+func / (vector: SCNVector3, scalar: Float) -> SCNVector3 {
+    return SCNVector3Make(vector.x / scalar, vector.y / scalar, vector.z / scalar)
+}
+
+/**
+ * Divides the x, y and z of a SCNVector3 by the same scalar value.
+ */
+func /= ( vector: inout SCNVector3, scalar: Float) {
+    vector = vector / scalar
+}
+
+/**
+ * Negate a vector
+ */
+func SCNVector3Negate(vector: SCNVector3) -> SCNVector3 {
+    return vector * -1
+}
+
+/**
+ * Returns the length (magnitude) of the vector described by the SCNVector3
+ */
+func SCNVector3Length(vector: SCNVector3) -> Float
+{
+    return sqrtf(vector.x*vector.x + vector.y*vector.y + vector.z*vector.z)
+}
+
+/**
+ * Returns the distance between two SCNVector3 vectors
+ */
+func SCNVector3Distance(vectorStart: SCNVector3, vectorEnd: SCNVector3) -> Float {
+    return SCNVector3Length(vector: vectorEnd - vectorStart)
+}
+
+/**
+ * Returns the distance between two SCNVector3 vectors
+ */
+func SCNVector3Normalize(vector: SCNVector3) -> SCNVector3 {
+    return vector / SCNVector3Length(vector: vector)
+}
+
+/**
+ * Calculates the dot product between two SCNVector3 vectors
+ */
+func SCNVector3DotProduct(left: SCNVector3, right: SCNVector3) -> Float {
+    return left.x * right.x + left.y * right.y + left.z * right.z
+}
+
+/**
+ * Calculates the cross product between two SCNVector3 vectors
+ */
+func SCNVector3CrossProduct(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.y * right.z - left.z * right.y, left.z * right.x - left.x * right.z, left.x * right.y - left.y * right.x)
+}
+
+/**
+ * Calculates the SCNVector from lerping between two SCNVector3 vectors
+ */
+func SCNVector3Lerp(vectorStart: SCNVector3, vectorEnd: SCNVector3, t: Float) -> SCNVector3 {
+    return SCNVector3Make(vectorStart.x + ((vectorEnd.x - vectorStart.x) * t), vectorStart.y + ((vectorEnd.y - vectorStart.y) * t), vectorStart.z + ((vectorEnd.z - vectorStart.z) * t))
+}
+
+/**
+ * Project the vector, vectorToProject, onto the vector, projectionVector.
+ */
+func SCNVector3Project(vectorToProject: SCNVector3, projectionVector: SCNVector3) -> SCNVector3 {
+    let scale: Float = SCNVector3DotProduct(left: projectionVector, right: vectorToProject) / SCNVector3DotProduct(left: projectionVector, right: projectionVector)
+    let v: SCNVector3 = projectionVector * scale
+    return v
+}
+extension SCNNode {
+    static func lineNode(from: SCNVector3, to: SCNVector3, radius: CGFloat = 0.25) -> SCNNode {
+        let vector = to - from
+        let height = vector.length()
+        let cylinder = SCNCylinder(radius: radius, height: CGFloat(height))
+        cylinder.radialSegmentCount = 4
+        let node = SCNNode(geometry: cylinder)
+        node.position = (to + from) / 2
+        node.eulerAngles = SCNVector3.lineEulerAngles(vector: vector)
+        return node
+    }
+}
+
+extension SCNVector3 {
+    static func lineEulerAngles(vector: SCNVector3) -> SCNVector3 {
+        let height = vector.length()
+        let lxz = sqrtf(vector.x * vector.x + vector.z * vector.z)
+        let pitchB = vector.y < 0 ? Float.pi - asinf(lxz/height) : asinf(lxz/height)
+        let pitch = vector.z == 0 ? pitchB : sign(vector.z) * pitchB
+        
+        var yaw: Float = 0
+        if vector.x != 0 || vector.z != 0 {
+            let inner = vector.x / (height * sinf(pitch))
+            if inner > 1 || inner < -1 {
+                yaw = Float.pi / 2
+            } else {
+                yaw = asinf(inner)
+            }
+        }
+        return SCNVector3(CGFloat(pitch), CGFloat(yaw), 0)
+    }
+}
 
 class ARViewController : UIViewController {
     
@@ -30,16 +269,31 @@ class ARViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for line in self.lines {
+        /*let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3(0, 0, -0.2)
+        
+        self.sceneView.scene.rootNode.addChildNode(boxNode)
+        */
+        
+        
+        /*for line in self.lines {
+            
             let startPoint = SCNVector3(x: (line?.start.x)!, y: (line?.start.y)!, z: (line?.start.z)!)
             let endPoint = SCNVector3(x: (line?.end.x)!, y: (line?.end.y)!, z: (line?.end.z)!)
+            let line = SCNNode.lineNode(from: startPoint, to: endPoint)
             
-            let line = SCNGeometry.line(from: startPoint, to: endPoint)
+            sceneView.scene.rootNode.addChildNode(line)
+            
+            /*let line = SCNGeometry.line(from: startPoint, to: endPoint)
             let lineNode = SCNNode(geometry: line)
-            
+            lineNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
             lineNode.position = SCNVector3Zero
             sceneView.scene.rootNode.addChildNode(lineNode)
-        }
+            glLineWidth(20)*/
+        }*/
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -65,12 +319,21 @@ class ARViewController : UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func render() {
+        let cyl = SCNCylinder(radius: 0.2, height: 1)
+        
+        let node = SCNNode(geometry: cyl)
+        node.position = SCNVector3Zero
+        
+        sceneView.scene.rootNode.addChildNode((node))
+    }
+    
     func loadLines()
     {
-        let planquery = PFQuery(className: "Line")
+        let linequery = PFQuery(className: "Line")
         //roomquery.whereKeyExists("planId")
-        planquery.whereKey("roomId", contains: String(self.roomnumber))
-        planquery.findObjectsInBackground ( block: { (lines, error) in
+        linequery.whereKey("roomId", contains: String(self.roomnumber))
+        linequery.findObjectsInBackground ( block: { (lines, error) in
             
             //print(rooms[0]["number"])
             if error == nil {
