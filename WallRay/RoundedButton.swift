e3581d1a4a830a0b926b5cff0edd5fe83d4450d9
Felix <felix_ohlsen@yahoo.de>
2018-06-25 18:14:24 +0200

    Icon, Raumansicht

---
+++ WallRay/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  WallRay
+//
+//  Created by Felix Ohlsen on 24.06.18.
+//  Copyright © 2018 Fritz Oppelt. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ WallRay/statsCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  roomCollectionViewCell.swift
+//  WallRay
+//
+//  Created by Felix Ohlsen on 24.06.18.
+//  Copyright © 2018 Fritz Oppelt. All rights reserved.
+//
+
+import UIKit
+
+class changelogCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellLabelName: UILabel!
+    @IBOutlet weak var cellLabelDate: UILabel!
+    @IBOutlet weak var cellLabelStatus: UILabel!
+}
